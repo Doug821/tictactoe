90776c68e37bc77ea95419a3f6f5af85b6cdfa4b
Doug821 <dougsilva821@gmail.com>
2021-07-25 12:03:22 -0300

    TicTacToe Game

---
+++ scripts/animation.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+    window.setTimeout(() => {
+        let loadingElement = document.querySelector('.loading');
+        loadingElement.style.height = 0;
+    }, 5000);
+    window.setTimeout(() => {
+        let tacToeElements = document.querySelector('.tacToeIcons');
+        tacToeElements.style.transform = "translateY(-50px)";
+        tacToeElements.style.height = 0;
+    }, 5800);
+});
+
+
+function next() {
+    let inputValue = document.querySelector('#gameMode2');
+    if (inputValue.checked) {
+        let section1 = document.querySelector('.section1');
+        let section2 = document.querySelector('.section2');
+
+        section1.style.height = 0;
+        section2.style.height = '100%';
+    } else {
+        alert('Escolha um modo de jogo');
+    }
+}
+
+function startGame() {
+    window.location = "/game.html";
+}
+
+let players = document.querySelectorAll('input[name="avatar"]');
+console.log(players);
+
+players.forEach((player) => {
+    player.addEventListener('click', () => {
+        playerSelection(player);
+    });
+})
+
+
+function playerSelection(player) {
+    if (player.id === 'avatar1') {
+        localStorage.clear();
+        localStorage.setItem('player1', 0);
+    } else if (player.id === 'avatar2') {
+        localStorage.clear();
+        localStorage.setItem('player1', 1);
+    }
+}
---
+++ scripts/interface.js
@@ -0,0 +1,84 @@
+let playerScore = {
+    player1: 0,
+    player2: 0
+}
+
+let gamePlayed = 0;
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    let squares = document.querySelectorAll(".square");
+
+    squares.forEach((square) => {
+        square.addEventListener('click', handleClick);
+    })
+
+})
+
+function handleClick(event) {
+
+
+    let square = event.target;
+    let postion = square.id;
+
+
+    if (handleMove(postion)) {
+        let winner = playerTime === 0 ? 'Jogador 1' : 'Jogador 2';
+
+        setTimeout(() => {
+            alert(" O Jogo Acabou - O Vencedor foi o " + winner);
+        }, 100);
+        updateScore(playerTime);
+    };
+    updateSquare(postion);
+}
+
+function updateSquare(postion) {
+    let square = document.getElementById(postion.toString());
+    let symbol = board[postion];
+    symbol == 'o' ?
+        square.innerHTML = `<img class='${symbol}' src="Assets/loadingBall.svg" onload="SVGInject(this)" alt="">` :
+        square.innerHTML = `<img  class='${symbol}' src="Assets/loadingCross.svg" onload="SVGInject(this)" alt="">`
+}
+
+function updateScore(playerThatScores) {
+    playerThatScores == 0 ?
+        playerScore.player1++ : playerScore.player2++;
+
+    let p1Score = document.querySelector('.realP1Score');
+    let p2Score = document.querySelector('.realP2Score');
+
+    p1Score.innerHTML = playerScore.player1;
+    p2Score.innerHTML = playerScore.player2;
+
+}
+
+let restartGame = document.querySelector(".restartGame");
+
+restartGame.addEventListener("click", () => {
+    let squares = document.querySelectorAll(".square");
+
+    squares.forEach((square) => {
+        let postion = square.id;
+        let symbol = board[postion];
+
+        if (symbol != '') {
+            square.innerHTML = ``;
+        }
+    })
+
+    board = [];
+    emptyBoard.map(() => {
+        board.push("");
+    })
+    gameOver = false;
+
+    playerTime = Math.random(0, 1);
+    playerTime >= 0.5 ? playerTime = 1 : playerTime = 0;
+});
+
+let goHome = document.querySelector('.home');
+
+goHome.addEventListener('click', () => {
+    window.location.href = '/';
+});
